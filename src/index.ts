a3517214e77557db5fe3a5c0090bdb21a0e1a837
Bahman World <bahman.world@gmail.com>
2025-07-16 00:47:33 +0330

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
-export type { DibapressConfig, DibapressApp } from "./Dibapress";
 export { default as Dibapress } from "./Dibapress";
+export type { DibapressConfig, DibapressApp } from "./Dibapress";
 export { GET, POST, PUT, DELETE } from "./routes/dibapress";
